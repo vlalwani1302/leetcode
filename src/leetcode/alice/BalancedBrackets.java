12467b1a0498b5c0775e6b579c0b73628a568e6d
CharuUpadhyay <59087151+CharuUpadhyay@users.noreply.github.com>
2020-08-25 15:17:46 +0100

    Add files via upload

---
+++ src/leetcode/alice/BalancedBrackets.java
@@ -0,0 +1,72 @@
+
+public class BalancedBrackets {
+	public static void main(String[] args) {
+		//isBalanced("{[()]}");
+			System.out.println(isBalanced(""
+					+ "{)[])}]){){]}[(({[)[{{[((]{()[]}][([(]}{](])()(}{(]}{})[)))[](){({)][}()((" 
+					)
+			);/*
+			 * isBalanced("[](){()}"); isBalanced("()"); isBalanced("({}([][]))[]()");
+			 * isBalanced("{)[](}]}]}))}(())("); isBalanced("([[)");
+			 */
+	}
+
+	
+	/*
+	 * 40 ( 41 ) 91 [ 93 ] 123 { 125 }
+	 */
+	static String isBalanced(String s) {
+		java.util.Stack stack = new java.util.Stack();
+		char[] charArray = s.toCharArray();
+		for (char c : charArray) {
+			switch (c) {
+			case '{':
+			case '[':
+			case '(':
+				stack.push(c);
+				break;
+			case ')':
+				if (!stack.isEmpty()) {
+					char lastChar = (char) stack.peek();
+					int lastIntVal = (int) lastChar;
+					
+					if (lastIntVal != 40) {
+						return "NO";
+					}
+					stack.pop();
+				}else {
+					return "NO";
+				}
+				break;
+			case ']':
+				if (!stack.isEmpty()) {
+					char lastChar = (char) stack.peek();
+					int lastIntVal = (int) lastChar;
+					if (lastIntVal != 91) {
+						return "NO";
+					}
+					stack.pop();
+
+				}else {
+					return "NO";
+				}
+				break;
+			case '}':
+				if (!stack.isEmpty()) {
+					char lastChar = (char) stack.peek();
+					int lastIntVal = (int) lastChar;
+					if (lastIntVal != 123) {
+						return "NO";
+					}
+					stack.pop();
+
+				}else {
+					return "NO";
+				}
+				break;
+
+			}
+		}
+		return "YES";
+	}
+}
---
+++ src/leetcode/alice/TwoStacksQueue.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class TwoStacksQueue {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. FIFO*/ 
+        Scanner scanner = new Scanner(System.in);
+        String[] nd = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(nd[0]);
+
+            Stack stackA = new Stack();
+            Stack stackB = new Stack();
+
+            for(int i = 0; i < n ; i++){
+               String[] str = scanner.nextLine().split(" ");
+               int query = 0;
+               int data = 0;
+               query = Integer.parseInt(str[0]);
+               if(str.length == 2) {
+                   data = Integer.parseInt(str[1]);
+               }
+
+               switch(query){
+               case 1: //enqueue element in queue
+                   stackA.push(data);
+                   break;
+               case 2: // dequeue element from queue
+                   
+                   if(stackB.isEmpty()){
+                    while(!stackA.isEmpty()){
+                       stackB.push(stackA.pop());
+                    }
+                   }
+                   
+                   stackB.pop();
+                   
+                   break;
+               case 3: // print first element
+                   if(stackB.isEmpty()){
+
+                    while(!stackA.isEmpty()){
+                        stackB.push(stackA.pop());
+                    }
+                   }
+                   System.out.println(stackB.peek());
+                   
+                   break;
+               }
+            }
+        }
+}
