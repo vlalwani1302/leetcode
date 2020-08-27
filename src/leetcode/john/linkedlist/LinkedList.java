2e008e4f58b1a67277e9515126fc46147eb5f800
vishal <vlalwani1302@gmail.com>
2020-08-27 15:54:35 +0530

    remove nth node linked list

--- src/leetcode/john/linkedlist/LinkedList.java
+++ src/leetcode/john/linkedlist/LinkedList.java
@@ -19,7 +19,7 @@ public class LinkedList {
 	public void show() {
 		Node temp = head;
 		while(temp.next != null) {
-			System.out.println(temp.data);
+			System.out.print(temp.data);
 			temp = temp.next;
 		}
 		System.out.println(temp.data);
@@ -69,5 +69,71 @@ public class LinkedList {
 		}
 		newNode.next = current.next;
 		current.next = newNode;
+	}
+	public Node removeNthFromEnd(Node head, int n) {
+		Node temp1 = head;
+		Node temp2 = head;
+		Node result;
+		int size = 1;
+		// find length of list
+		while(temp1.next != null) {
+			temp1 = temp1.next;
+			size++;
+		}
+		// traverse to length-n
+		for(int i = 1; i < size - n; i++) {
+			temp2 = temp2.next;
+		}
+		result = temp2.next;
+		temp2.next = temp2.next.next;
+		return result;
+	}
+	
+//	public Node removeNthFromEnd2(Node head, int n) {
+//		Node fast = head;
+//		Node slow = head;
+//		Node result = null;
+//		for(int i = 0; i < n; i++) {
+//			fast = fast.next;
+//		}
+//		while(fast.next != null) {
+//			fast = fast.next;
+//			slow = slow.next;
+//		}
+//		result = slow.next;
+//		slow.next = slow.next.next;
+//		return result;
+//	}
+	
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.append(1);
+		list.append(2);
+		list.append(3);
+		list.append(4);
+		list.append(5);
+		
+		System.out.println("Before:");
+		list.show();
+		list.removeNthFromEnd(list.head, 2);
+		
+		System.out.println("After:");
+		list.show();
+		
+//		// For other solution 
+//		System.out.println("For other solution:\n");
+//		LinkedList list2 = new LinkedList();
+//		list2.append(1);
+//		list2.append(2);
+//		list2.append(3);
+//		list2.append(4);
+//		list2.append(5);
+//		
+//		System.out.println("Before:");
+//		list2.show();
+//		list2.removeNthFromEnd2(list.head, 2);
+//		
+//		System.out.println("After:");
+//		list2.show();
 	}
 }
