7237d6cc995acd48eb9972a25f456bd93fa60c88
Charu Upadhyay <charuupadhyay04@gmail.com>
2020-09-04 22:58:11 +0100

    uniquePaths

--- src/leetcode/alice/array/UniquePaths.java
+++ src/leetcode/alice/array/UniquePaths.java
@@ -6,6 +6,8 @@ class UniquePaths {
 
 		Integer result1 = uniquePaths(51, 9);
 		System.out.println(result1);
+		
+		System.out.println(uniquePathsDP(51, 9));
 	}
 
 	public static int uniquePaths(int m, int n) {
@@ -14,6 +16,26 @@ class UniquePaths {
 			return 1;
 		}
 		return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+	}
+
+	public static int uniquePathsDP(int m, int n) {
+
+		int[][] count = new int[m][n];
+
+		for (int i = 0; i < m; i++) {
+			count[i][0] = 1;
+		}
+		for (int i = 0; i < n; i++) {
+			count[0][i] = 1;
+		}
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				count[i][j] = count[i-1][j] + count[i][j-1];
+			}
+		}
+		
+		return count[m-1][n-1];
 	}
 
 }
