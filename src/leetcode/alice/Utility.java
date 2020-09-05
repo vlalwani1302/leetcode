48f45480a85f60c78a55e9eed4bf866fc1d37b71
Charu Upadhyay <charuupadhyay04@gmail.com>
2020-09-05 14:54:58 +0100

    Utility.java

--- src/leetcode/alice/Utility.java
+++ src/leetcode/alice/Utility.java
@@ -2,9 +2,18 @@ package leetcode.alice;
 
 public class Utility {
 
-	public static void printArray(int[] result){
+	public static void printArray(int[] result) {
 		for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
-        }
+			System.out.print(result[i] + " ");
+		}
+	}
+
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
