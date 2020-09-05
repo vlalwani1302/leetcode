d568eb96ec0ac508c22d7b48c7bb905901a585d3
Charu Upadhyay <charuupadhyay04@gmail.com>
2020-09-05 15:36:46 +0100

    SetMatrixZeroes.java

--- src/leetcode/alice/array/SetMatrixZeroes.java
+++ src/leetcode/alice/array/SetMatrixZeroes.java
@@ -10,6 +10,11 @@ class SetMatrixZeroes {
 		int[][] matrix = { { 0, 1, 2, 0 }, { 3, 4, 5, 2 }, { 1, 3, 1, 5 } };
 		setZeroes(matrix);
 		Utility.printMatrix(matrix);
+
+		int[][] matrix1 = { { 0, 1, 2, 0 }, { 3, 4, 5, 2 }, { 1, 3, 1, 5 } };
+		setZeroes1(matrix1);
+		Utility.printMatrix(matrix1);
+
 	}
 
 	public static void setZeroes(int[][] matrix) {
@@ -45,4 +50,36 @@ class SetMatrixZeroes {
 			}
 		}
 	}
+
+	// Space complexity - O(1)
+	public static void setZeroes1(int[][] matrix) {
+		int newValue = -2147483647;
+		if (matrix.length > 0) {
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[i].length; j++) {
+					if (matrix[i][j] == 0) {
+						// mark entire row as something else
+						for (int r = 0; r < matrix.length; r++) {
+							if(matrix[r][j] != 0)
+								matrix[r][j] = newValue;
+						}
+						// mark entire column as something else
+						for (int c = 0; c < matrix[i].length; c++) {
+							if(matrix[i][c] != 0)
+								matrix[i][c] = newValue;
+						}
+					}
+				}
+			}
+
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[i].length; j++) {
+					if (matrix[i][j] == newValue) {
+						matrix[i][j] = 0;
+					}
+				}
+			}
+		}
+	}
+
 }
