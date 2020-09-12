aec11ebabdfaf790f3212d27e42daf6b289f3443
vishal <vlalwani1302@gmail.com>
2020-09-12 18:26:57 +0530

    InvertABinaryTree

---
+++ src/leetcode/john/easy/tree/InvertABinaryTree.java
@@ -0,0 +1,30 @@
+package leetcode.john.easy.tree;
+
+import leetcode.john.BinaryTree;
+import leetcode.john.TreeNode;
+
+public class InvertABinaryTree {
+	public static TreeNode invertTree(TreeNode root) {
+		if(root == null) {
+			return root;
+		}
+		TreeNode left = invertTree(root.left);
+		TreeNode right = invertTree(root.right);
+		
+		root.left = right;
+		root.right = left;
+		return root;
+    }
+    
+    public static void main(String[] args) {
+    	BinaryTree tree1 = new BinaryTree();
+    	tree1.insert(1); 
+        tree1.insert(2); 
+        tree1.insert(3); 
+        tree1.insert(4); 
+        tree1.insert(5); 
+        
+    	
+    	System.out.println(maxDepth(tree1.getRoot()));
+    }
+}
