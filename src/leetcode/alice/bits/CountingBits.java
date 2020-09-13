e73f600147d46ae977a9802a1597b9737a52f3ef
Charu Upadhyay <charuupadhyay04@gmail.com>
2020-09-13 11:51:57 +0100

    edit

--- src/leetcode/alice/bits/CountingBits.java
+++ src/leetcode/alice/bits/CountingBits.java
@@ -1,23 +1,42 @@
 package leetcode.alice.bits;
+
 class CountingBits {
 	public static void main(String[] args) {
 		countBits(4);
 	}
+
 	public static int[] countBits(int num) {
-        if(num == 0)
-            return new int[]{0};
-        
-        int[] result = new int[num];
-        result[0] = 0;
-        for(int i=1; i<=num; i++){
-            int count = 0;
-            while(i != 0){
-                count++;
-                i = i & (i-1);
-            }
-            result[i] = count;
-        }
-        
-        return result;
-    }
+		int[] result = new int[num + 1];
+		for (int i = 0; i <= num; i++) {
+			int count = 0;
+			int n = i;
+			while (n != 0) {
+				count++;
+				n = n & (n - 1);
+			}
+			result[i] = count;
+		}
+
+		return result;
+	}
+
+	public int[] countBits1(int num) {
+		int[] result = new int[num + 1];
+		result[0] = 0;
+
+		for (int i = 1; i <= num; i++) {
+			int bitCount = 0;
+
+			bitCount += countSetBitsUtil(i);
+			result[i] = bitCount;
+		}
+
+		return result;
+	}
+
+	public int countSetBitsUtil(int x) {
+		if (x <= 0)
+			return 0;
+		return (x % 2 == 0 ? 0 : 1) + countSetBitsUtil(x / 2);
+	}
 }
