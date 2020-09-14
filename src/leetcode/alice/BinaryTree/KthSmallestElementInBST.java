04692db3f4a55c19a6ccc139376752103e367210
Charu Upadhyay <charuupadhyay04@gmail.com>
2020-09-14 14:21:10 +0100

    KthSmallestElementInBST

---
+++ src/leetcode/alice/BinaryTree/KthSmallestElementInBST.java
@@ -0,0 +1,39 @@
+package leetcode.alice.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class KthSmallestElementInBST {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> arr = new ArrayList<Integer>();
+            arr = preorder(root, arr);
+        
+        return arr.get(k-1);
+    }
+    
+    List<Integer> preorder(TreeNode root, List<Integer> arr ){
+        if(root == null){
+            return arr;
+        }
+        preorder(root.left, arr);
+        arr.add(root.val);
+        preorder(root.right, arr);
+        
+        return arr;
+    }
+}
