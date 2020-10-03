5a3d5f51f1bf077bbee0b33ec487252748a7c281
Charu Upadhyay <charuupadhyay04@gmail.com>
2020-10-03 11:14:57 +0100

    edit

--- src/leetcode/alice/array/FindPeakElement.java
+++ src/leetcode/alice/array/FindPeakElement.java
@@ -34,7 +34,31 @@ class FindPeakElement {
 		}
 	}
 
+	public static int findPivot(int arr[], int low, int high) {
+		// base cases
+		if (high < low)
+			return -1;
+		if (high == low)
+			return low;
+
+		/* low + (high - low)/2; */
+		int mid = (low + high) / 2;
+		if (mid < high && arr[mid] > arr[mid + 1])
+			return mid;
+		if (mid > low && arr[mid] < arr[mid - 1])
+			return (mid - 1);
+		if (arr[low] >= arr[mid])
+			return findPivot(arr, low, mid - 1);
+		return findPivot(arr, mid + 1, high);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(findPeakElement(new int[] { 1, 2, 3 }));
+		System.out.println(findPeakElement(new int[] { 1, 2 }));
+		System.out.println(findPivot(new int[] { 1, 2 }, 0, 1));
+		
+		int[] nums = new int[] {7,8,1,2,3,4,5,6};
+		System.out.println(findPeakElement(nums));
+		System.out.println(findPivot(nums,0,nums.length-1));
+
 	}
 }
--- src/leetcode/alice/array/SearchInRotatedSortedArray.java
+++ src/leetcode/alice/array/SearchInRotatedSortedArray.java
@@ -17,62 +17,64 @@ public class SearchInRotatedSortedArray {
 	public static void main(String[] args) {
 		int[] arr = new int[] { 4, 5, 6, 7, 0, 1, 2 };
 		System.out.println(search(arr, 0));
-		System.out.println(searchBS(new int[] {1,3}, 1));
-		System.out.println(searchBS(new int[] {5,1,3}, 3));
-		System.out.println(searchBS(new int[] {4,5,6,7,8,1,2,3}, 8));
-
+		System.out.println(searchBS(new int[] { 1, 3 }, 1));
+		System.out.println(searchBS(new int[] { 5, 1, 3 }, 1));
+		System.out.println(searchBS(new int[] { 4, 5, 6, 7, 8, 1, 2, 3 }, 8));
+		
+		
+		System.out.println(searchBS(new int[] { 7,8,1,2,3,4,5,6},2));
 
 	}
 
 	public static int searchBS(int[] nums, int target) {
-        int n = nums.length;
-       int pivot = findPivot(nums, 0, n - 1); 
-  
-        // If we didn't find a pivot, then 
-        // array is not rotated at all 
-        if (pivot == -1) 
-            return binarySearch(nums, 0, n - 1, target); 
-  
-        // If we found a pivot, then first 
-        // compare with pivot and then 
-        // search in two subarrays around pivot 
-        if (nums[pivot] == target) 
-            return pivot; 
-        if (nums[0] <= target) 
-            return binarySearch(nums, 0, pivot - 1, target); 
-        return binarySearch(nums, pivot + 1, n - 1, target); 
-    }
-    
-    public static int findPivot(int arr[], int low, int high) 
-    { 
-        // base cases 
-        if (high < low) 
-            return -1; 
-        if (high == low) 
-            return low; 
-  
-        /* low + (high - low)/2; */
-        int mid = (low + high) / 2; 
-        if (mid < high && arr[mid] > arr[mid + 1]) 
-            return mid; 
-        if (mid > low && arr[mid] < arr[mid - 1]) 
-            return (mid - 1); 
-        if (arr[low] >= arr[mid]) 
-            return findPivot(arr, low, mid - 1); 
-        return findPivot(arr, mid + 1, high); 
-    } 
-    
-   static int binarySearch(int arr[], int low, int high, int key) 
-    { 
-        if (high < low) 
-            return -1; 
-  
-        /* low + (high - low)/2; */
-        int mid = (low + high) / 2; 
-        if (key == arr[mid]) 
-            return mid; 
-        if (key > arr[mid]) 
-            return binarySearch(arr, (mid + 1), high, key); 
-        return binarySearch(arr, low, (mid - 1), key); 
-    }
+		int n = nums.length;
+		int pivot = findPivot(nums, 0, n - 1);
+
+		// If we didn't find a pivot, then
+		// array is not rotated at all
+		if (pivot == -1)
+			return binarySearch(nums, 0, n - 1, target);
+
+		// If we found a pivot, then first
+		// compare with pivot and then
+		// search in two subarrays around pivot
+		if (nums[pivot] == target)
+			return pivot;
+		if (nums[0] <= target)
+			return binarySearch(nums, 0, pivot - 1, target);
+		return binarySearch(nums, pivot + 1, n - 1, target);
+	}
+
+	public static int findPivot(int arr[], int low, int high) {
+		// base cases
+		if (high < low)
+			return -1;
+		if (high == low)
+			return low;
+
+		/* low + (high - low)/2; */
+		int mid = (low + high) / 2;
+		if (mid < high && arr[mid] > arr[mid + 1])
+			return mid;
+		if (mid > low && arr[mid] < arr[mid - 1])
+			return (mid - 1);
+		if (arr[low] >= arr[mid])
+			return findPivot(arr, low, mid - 1);
+		return findPivot(arr, mid + 1, high);
+	}
+
+	static int binarySearch(int arr[], int low, int high, int key) {
+		if (high < low)
+			return -1;
+
+		/* low + (high - low)/2; */
+		int mid = (low + high) / 2;
+		if (key == arr[mid])
+			return mid;
+		if (key > arr[mid])
+			return binarySearch(arr, (mid + 1), high, key);
+		return binarySearch(arr, low, (mid - 1), key);
+	}
+
+		
 }
